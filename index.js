45bcad49616fbc4d3978a63c608aad1445f3ea49
Lipton Barua <liptonbarua274@gmail.com>
2022-10-26 02:07:37 +0600

    project -4

--- index.js
+++ index.js
@@ -13,7 +13,15 @@ app.get('/categories-list', (req, res)=>{
 });
 
 app.get('/courses/:id', (req, res)=>{
-    console.log(req.params.id)
+    const id=req.params.id;
+    const selectedCourse = courses.find(course=>course.id===id);
+    res.send(selectedCourse);
+});
+
+app.get('/categorie/:id', (req, res)=>{
+    const id = req.params.id;
+    const categorie_id = courses.filter(c=>c.courses_name===id);
+    res.send(categorie_id);
 })
 
 app.listen(port, ()=>{
